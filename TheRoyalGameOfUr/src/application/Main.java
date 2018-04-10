3a200d265df818931f9bcfc7c4451c29921c0d94
Jared Andrzejewski <jared.andrz@gmail.com>
2018-04-10 07:57:22 -0500

    Updated Dice and Main class

--- TheRoyalGameOfUr/src/application/Main.java
+++ TheRoyalGameOfUr/src/application/Main.java
@@ -1,20 +1,25 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			// Load the FXML file for the game board
+			Parent root = FXMLLoader.load(getClass().getResource("/Gameboard.fxml"));
+			
+			// Set the scene onto the stage
+			primaryStage.setScene(new Scene(root, 900, 900));
+			
+			// Display the board to the user
 			primaryStage.show();
+						
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- TheRoyalGameOfUr/src/application/model/Dice.java
+++ TheRoyalGameOfUr/src/application/model/Dice.java
@@ -1,5 +1,17 @@
 package application.model;
 
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+	
+	public static int rollDice() {
+		int rollTotal = 0;
+		int rollCounter = 0;
+		Random rand = new Random();
+		for (int i = 0; i < 4; i++) {
+			rollCounter = rand.nextInt(2);
+			rollTotal += rollCounter;
+		}
+		return rollTotal;
+	}
 }
